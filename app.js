1670adab934d9df8aaaa7e652e448731090fae66
samueljustus <samie.justus@gmail.com>
2023-10-16 08:32:31 +0100

    changes made in css

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const hamburgerMenu = document.querySelector('.hamburger')
 const menuItem = document.querySelector('.menu-items')
 
 hamburgerMenu.addEventListener('click', () => {
-    
+    menuItem.classList.toggle('visible');
 })
