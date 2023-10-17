539c856b0fe93a660b3cdb288ada626399cdc487
samueljustus <samie.justus@gmail.com>
2023-10-17 11:00:31 +0100

    responsiveness

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const hamburgerMenu = document.querySelector('.hamburger')
 const menuItem = document.querySelector('.menu-items')
+const mainBody = document.querySelector('.container')
 
 hamburgerMenu.addEventListener('click', () => {
     menuItem.classList.toggle('visible');
+
 })
